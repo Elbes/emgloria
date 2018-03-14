7c0f113dc4c75aa56936a56dc0d2e2a4efc958ab
Elbes <elbes2009@gmail.com>
2018-03-13 22:28:52 -0300

    Nova alteração na galeria

--- resources/views/galeria.blade.php
+++ resources/views/galeria.blade.php
@@ -1,9 +1,10 @@
 @extends('layout.layoutSite')
 @section('dependencyCss')
-
-<link rel="stylesheet" href="{{ url('/layout/') }}/css/jPages.css" type="text/css" media="all"/>
-<link rel="stylesheet" href="{{ url('/layout/') }}/css/animate.css" type="text/css" media="all"/>
-	
+<!-- Start JavaScript -->
+<link rel="stylesheet" href="{{ url('/layout/') }}/css/javascri.css" type="text/css" media="all">
+<!-- gallery CSS -->
+<link rel="stylesheet" href="{{ url('/layout/') }}/css/prettyPhoto.css" type="text/css" media="all">
+<!-- Start Player CSS -->
 @endsection
 @section('content')
  <!-- Content -->
@@ -20,90 +21,41 @@
 	        <span class="detail">Um pouco dos nossos momentos traduzidos em imagens. Venha conhecer pessoalmente.</span>
 	   </article>
 	   	
-	   <!-- <article class="banner-bottom-slide">
-
-        
-        <div id="img" class="animated"><div class="img-holder"><img src="{{ url('/imagens/galeria/') }}/{{ $img_primeira->nome_imagem }}" alt="image" class="img-responsive imagem-amor"/></div></div>
-
-       
-        <ul id="thumbs" class="clearfix">
-            @foreach ($imagens as $imagem)
-            <li class="img-holder"><div class="img-holder"><img src="{{ url('/imagens/galeria/') }}/{{ $imagem->nome_imagem }}" alt="image" class="img-responsive imagem-amor"/></div></li>
-            @endforeach
-        </ul>
-
-       
-        <div class="holder"></div>
-
-       
-        <div id="btns">
-            <span class="prev"></span>
-            <span class="next"></span>
-        </div>
-
-
-  	   </article> -->
-  	   
-  	   <section class="grid-holder">
+	   	<section class="grid-holder">
         <section class="grid">
          
-          @foreach ($imagens as $imagem)
-	          <article class="column c-one-third">
-	            <div class="video-holder slide-1"><img class="video" src="{{ url('/imagens/galeria/') }}/{{ $imagem->nome_imagem }}" alt="image" class="img-responsive imagem-amor"/></div>
-	          </article>
-          @endforeach
+          <article class="column c-three-fourth">
+            <section class="grid-holder">
+              <section class="grid lightbox gallery" id="catagory-item-holder">
+              
+              @foreach ($imagens as $imagem)
+                <figure class="column c-one-fourth gallery-col Landscape catagory-item">
+                  <div class="item alpha gallery-item">
+                    <div class="caption gallery-box"> <a href="{{ url('/imagens/galeria/') }}/{{ $imagem->nome_imagem }}" rel="prettyPhoto[gallery1]"> <img src="{{ url('/imagens/galeria/') }}/{{ $imagem->nome_imagem }}" alt="" class="pic"> <span class="hover-effect big zoom"></span></a></div>
+                    
+                    <!-- hover effect --> 
+                    
+                    <strong class="gallery-titel"><a href="gallery-singlepost.html">Our Churches</a></strong>
+                    <p>Sit amet, consectetur adipiscing </p>
+                    <strong></strong>
+                  </div>
+                  
+                  <!-- End --> 
+                  
+                </figure>
+               @endforeach
+               
+              </section>
+            </section>
+            <br clear="all">
+            
+          </article>
         </section>
       </section>
-    
 
     </div>
 
 @endsection
 @section('dependencyJs')
-<script src="{{ url('/layout/') }}/js/paginacao/jPages.js"></script>
-<script src="{{ url('/layout/') }}/js/paginacao/jquery-1.8.2.min.js"></script>
-<script src="{{ url('/layout/') }}/js/paginacao/highlight.pack.js"></script>
-<script src="{{ url('/layout/') }}/js/paginacao/tabifier.js"></script>
-<script src="{{ url('/layout/') }}/js/paginacao/js.js"></script>
-<script src="{{ url('/layout/') }}/js/paginacao/jPages.min.js"></script>
- <script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-28718218-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-  </script>
-  
-    <script>
-  $(function() {
-
-    /* initiate plugin */
-    $("div.holder").jPages({
-      containerID : "thumbs",
-      perPage     : 4,
-      previous    : ".prev",
-      next        : ".next",
-      links       : "blank",
-      direction   : "auto",
-      animation   : "fadeInUp"
-    });
-
-    $("ul#thumbs li").click(function(){
-      $(this).addClass("selected")
-      .siblings()
-      .removeClass("selected");
-
-      var img = $(this).children().clone().addClass("animated fadeInDown");
-      $("div#img").html( img );
-
-    });
-
-  });
-  </script>
 @endsection
